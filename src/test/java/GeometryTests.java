aafed9e6959c3fcdf86ac3cf466d121159b38cfa
jethronap <efthioa@gmail.com>
2020-02-23 00:55:59 +0200

    added perimeter test

--- src/test/java/GeometryTests.java
+++ src/test/java/GeometryTests.java
@@ -5,18 +5,28 @@ import static org.junit.Assert.assertEquals;
 
 public class GeometryTests {
 
-    @Test
-    public void getTriangleArea(){
+    double sideA = 3.0;
+    double sideB = 5.0;
+    double sideC = 7.0;
 
-        double sideA = 3.0;
-        double sideB = 5.0;
-        double sideC = 7.0;
+    @Test
+    public void getTriangleArea() {
 
         Triangle ABC = new Triangle(sideA, sideB, sideC);
 
         double areaOfABC = ABC.area();
-        assertEquals(areaOfABC,6.4952, 1.0);
+        assertEquals(areaOfABC, 6.4952, 1.0);
 
+
+    }
+
+    @Test
+    public void getTrianglePerimeter() {
+
+        Triangle ABC = new Triangle(sideA, sideB, sideC);
+
+        double perimeterOfABC = ABC.perimeter();
+        assertEquals(perimeterOfABC, 15.0, 1.0);
 
     }
 }
