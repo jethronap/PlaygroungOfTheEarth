197afe6e3afead00e6492101a91890f238620e1c
jethronap <efthioa@gmail.com>
2020-06-28 19:49:57 +0300

    added demo for string builder illustration

---
+++ src/main/java/designPatterns/builder/Demo.java
@@ -0,0 +1,21 @@
+package designPatterns.builder;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // suppose we want to show a word in a web page
+        String hello = "hello";
+        System.out.println("<p>" + hello + "</p>");
+
+        //but what if we wanted to print an array of words
+        String[] words = {"hello", "world"};
+        // we could use a builder, which works piece by piece:
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<ul>\n");
+        for (String word: words) {
+            stringBuilder.append(String.format("  <li>%s</li>\n", word));
+        }
+        stringBuilder.append("</ul>");
+        System.out.println(stringBuilder);
+    }
+}
