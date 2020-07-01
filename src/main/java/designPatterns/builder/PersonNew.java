911ce5bd3278d4a36ae34df13ecb1064471cbb74
jethronap <efthioa@gmail.com>
2020-07-01 15:19:40 +0300

    added new Person class

---
+++ src/main/java/designPatterns/builder/PersonNew.java
@@ -0,0 +1,23 @@
+package designPatterns.builder;
+
+public class PersonNew {
+
+    // address
+    public String streetAddress, postcode, city;
+
+    // employment
+    public String companyName, position;
+    public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "PersonNew{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", city='" + city + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", annualIncome=" + annualIncome +
+                '}';
+    }
+}
