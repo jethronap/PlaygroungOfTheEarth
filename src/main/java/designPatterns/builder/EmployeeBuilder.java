61d080327cd742e251fe26dc6bdd5ea9fa3652a4
jethronap <efthioa@gmail.com>
2020-07-01 14:58:34 +0300

    used helper method & propagated fuent interface up the hierarchy

--- src/main/java/designPatterns/builder/EmployeeBuilder.java
+++ src/main/java/designPatterns/builder/EmployeeBuilder.java
@@ -5,7 +5,11 @@ public class EmployeeBuilder extends PersonBuilder<EmployeeBuilder> {
 
     public EmployeeBuilder worksAt(String position) {
         person.position = position;
-        return this;
+        return self();
     }
 
+    @Override
+    protected EmployeeBuilder self() {
+        return this;
+    }
 }
