87f10d49688d6ebac5bd4b4ab7bbc8eb3bf1e76f
jethronap <efthioa@gmail.com>
2020-07-01 14:50:40 +0300

    commented on return type argument

--- src/main/java/designPatterns/builder/EmployeeBuilder.java
+++ src/main/java/designPatterns/builder/EmployeeBuilder.java
@@ -1,5 +1,6 @@
 package designPatterns.builder;
-
+                                                // we provide EmployeeBuilder as argument here
+                                                // in order to preserve the fluent interface
 public class EmployeeBuilder extends PersonBuilder<EmployeeBuilder> {
 
     public EmployeeBuilder worksAt(String position) {
