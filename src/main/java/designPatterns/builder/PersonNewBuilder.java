ce4e5b916e21fef8f37decb68d0e9e2f9140ce67
jethronap <efthioa@gmail.com>
2020-07-01 15:38:14 +0300

    exposed job builder

--- src/main/java/designPatterns/builder/PersonNewBuilder.java
+++ src/main/java/designPatterns/builder/PersonNewBuilder.java
@@ -8,8 +8,14 @@ public class PersonNewBuilder {
     // the obj that gets stored
     protected PersonNew person = new PersonNew();
 
+    // expose address builder
     public PersonNewAddressBuilder lives() {
         return new PersonNewAddressBuilder(person);
+    }
+
+    // expose job builder
+    public PersonNewJobBuilder works() {
+        return new PersonNewJobBuilder(person);
     }
 
     // method to expose Person
