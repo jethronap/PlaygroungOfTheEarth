03be0f05ff384900e1a58cd71e1a399607923f21
jethronap <efthioa@gmail.com>
2020-07-01 15:32:25 +0300

    implemented address builder

--- src/main/java/designPatterns/builder/PersonNewBuilder.java
+++ src/main/java/designPatterns/builder/PersonNewBuilder.java
@@ -6,10 +6,14 @@ package designPatterns.builder;
 public class PersonNewBuilder {
 
     // the obj that gets stored
-    protected PersonNew person = new Person();
+    protected PersonNew person = new PersonNew();
+
+    public PersonNewAddressBuilder lives() {
+        return new PersonNewAddressBuilder(person);
+    }
 
     // method to expose Person
-    public Person build() {
+    public PersonNew build() {
         return person;
     }
 }
