83dfea275b6d817f4d44a27b87a8e5ebb589b861
jethronap <efthioa@gmail.com>
2020-07-05 13:38:08 +0300

    demonstrate factory method

---
+++ src/main/java/designPatterns/factory/Demo.java
@@ -0,0 +1,9 @@
+package designPatterns.factory;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // a new polar point would be created like so
+        Point polarPoint = Point.newPolarPoint(2, 3);
+    }
+}
