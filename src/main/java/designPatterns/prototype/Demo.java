ffa64b0aefaa9cc04056ef4a36f0bed9793b99fd
jethronap <efthioa@gmail.com>
2020-07-05 15:59:07 +0300

    implementation of copy through serialization

--- src/main/java/designPatterns/prototype/Demo.java
+++ src/main/java/designPatterns/prototype/Demo.java
@@ -1,5 +1,7 @@
 package designPatterns.prototype;
 
+import org.apache.commons.lang3.SerializationUtils;
+
 public class Demo {
     public static void main(String[] args) {
         Employee john = new Employee("John",
@@ -13,5 +15,21 @@ public class Demo {
         mary.address.streetAddress = "124 Somewhere Str";
         System.out.println(john);
         System.out.println(mary);
+
+        Foo foo = new Foo(666, "life");
+
+        /**
+         * making use of serialization utils from apache commons
+         * roundTrip() serializes the obj and then deserializes it
+         * in order to make a new copy, (something similar to copy by value
+         * it does it for the whole obj graph (ie. it has other obj inside)
+         */
+        Foo foo2 = SerializationUtils.roundtrip(foo);
+
+        foo2.whatever = "robots";
+
+        System.out.println(foo);
+        System.out.println(foo2);
     }
 }
+
