a949c7880600f7889d4c921860f3fb36a05ac4d7
jethronap <efthioa@gmail.com>
2020-07-07 20:36:45 +0300

    thread safe singleton impl

--- src/main/java/designPatterns/singleton/LazySingleton.java
+++ src/main/java/designPatterns/singleton/LazySingleton.java
@@ -13,11 +13,30 @@ public class LazySingleton {
         System.out.println("initializing lazy singleton");
     }
 
+    // for thread safety we can add synchronized
+    // to the signature of getInstance.
+    // `has performance issues`.
+//    public static LazySingleton getInstance() {
+//        // we check if the object is created
+//        // if not create it here
+//        if (instance == null) {
+//            instance = new LazySingleton();
+//        }
+//        return instance;
+//    }
+
+    /**
+     * double-checked locking
+     * thread-safe way to create a singleton
+     * OUTDATED (textbook iml)
+     */
     public static LazySingleton getInstance() {
-        // we check if the object is created
-        // if not create it here
         if (instance == null) {
-            instance = new LazySingleton();
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
         }
         return instance;
     }
