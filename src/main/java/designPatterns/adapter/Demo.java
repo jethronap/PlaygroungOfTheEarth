8596a57cbb7bbcf16a3bf84de677499198695389
jethronap <efthioa@gmail.com>
2020-07-19 15:59:31 +0300

    initialised vector rectangle

--- src/main/java/designPatterns/adapter/Demo.java
+++ src/main/java/designPatterns/adapter/Demo.java
@@ -1,6 +1,18 @@
 package designPatterns.adapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Demo {
+
+    // initialise vector rectangles inside a list of vector objects
+    private final static List<VectorObject> vectorObjects
+            = new ArrayList<>(Arrays.asList(
+                    new VectorRectangle(1,1,10,10),
+                    new VectorRectangle(2,2,20,20)
+    ));
+
     public static void main(String[] args) {
 
     }
