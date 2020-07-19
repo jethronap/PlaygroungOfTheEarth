4b8e61f1b5e69c26034af90663e92b7c6b0a0e15
jethronap <efthioa@gmail.com>
2020-07-19 19:10:09 +0300

    added vector renderer

---
+++ src/main/java/designPatterns/bridge/VectorRenderer.java
@@ -0,0 +1,9 @@
+package designPatterns.bridge;
+
+public class VectorRenderer implements Renderer {
+
+    @Override
+    public void renderCircle(float radius) {
+        System.out.println("Drawing a circle of radius " + radius);
+    }
+}
