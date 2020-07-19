370d0611cf5a4eb4cd72e9db2b057f638b644509
jethronap <efthioa@gmail.com>
2020-07-19 19:15:05 +0300

    added abstract class for bridge impl

---
+++ src/main/java/designPatterns/bridge/Shape.java
@@ -0,0 +1,16 @@
+package designPatterns.bridge;
+
+/**
+ * bridge pattern impl
+ */
+abstract class Shape {
+
+    protected Renderer renderer;
+
+    public Shape(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public abstract void draw();
+    public abstract void resize(float factor);
+}
