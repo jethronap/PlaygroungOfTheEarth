1e241361677e2fac2c7a2429a6b857e8d2b42f4b
jethronap <efthioa@gmail.com>
2020-07-19 19:38:15 +0300

    impl using google guice

--- src/main/java/designPatterns/bridge/Demo.java
+++ src/main/java/designPatterns/bridge/Demo.java
@@ -1,13 +1,25 @@
 package designPatterns.bridge;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class Demo {
     public static void main(String[] args) {
-        RasterRenderer raster = new RasterRenderer();
-        VectorRenderer vector = new VectorRenderer();
-
-        Circle circle = new Circle(vector, 5);
-        circle.draw();
-        circle.resize(2);
-        circle.draw();
+//        RasterRenderer raster = new RasterRenderer();
+//        VectorRenderer vector = new VectorRenderer();
+//
+//        Circle circle = new Circle(vector, 5);
+//        circle.draw();
+//        circle.resize(2);
+//        circle.draw();
+        /**
+         * impl using google guice
+         */
+        Injector injector = Guice.createInjector(new ShapeModule());
+        Circle instance = injector.getInstance(Circle.class);
+        instance.radius = 23;
+        instance.draw();
+        instance.resize(2);
+        instance.draw();
     }
 }
