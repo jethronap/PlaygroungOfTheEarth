4931cd9fb5955cf6df650be5068305a6c9c939b1
jethronap <efthioa@gmail.com>
2020-07-19 15:29:40 +0300

    added a line consisting of point

---
+++ src/main/java/designPatterns/adapter/Line.java
@@ -0,0 +1,11 @@
+package designPatterns.adapter;
+
+public class Line {
+
+    public Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+}
